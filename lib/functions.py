2c17cfbabc7ca03ae6a7a33a550502ecaad199aa
Kush <kuriaisac@gmail.com>
2024-08-27 13:52:53 +0300

    updates changes

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    
+    print(f'Hello, {name}!')
+
+greet('Naureen')
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
+
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    return sum
+
+add(45, 55)
+
 
 def halve(number):
-    pass
+    result = number / 2
+    return result
+halve(9)
